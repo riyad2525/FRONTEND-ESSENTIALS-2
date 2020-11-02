54fd1171ac46072a505bed172e0c045ef1544b93
Aldo Vogel <a.vogel@rocva.nl>
2020-11-02 20:11:45 +0100

    empty script

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,29 +1,2 @@
 let strNaam;
 let boolNaamKlopt;
-
-strNaam = prompt("Hoe heet je?");
-console.log(strNaam);
-
-boolNaamKlopt = confirm("Heet je " + strNaam +"?");
-console.log(boolNaamKlopt);
-
-if(boolNaamKlopt){
-    alert("Welkom " + strNaam + "!");
-    console.log("Naam klopt: " + strNaam);
-}
-else{
-    strNaam = prompt("Nog een keer: hoe heet je?");  
-    console.log("Naam klopt NIET: " + strNaam);
-    boolNaamKlopt = confirm("Heet je " + strNaam +"?");
-    if(boolNaamKlopt){
-        alert("Welkom " + strNaam + "!");
-        console.log("Naam klopt: " + strNaam);
-    }
-    else{
-        alert("Ok, evengoed Welkom!")
-        console.log("Naam klopt weer NIET: " + strNaam);
-    }
-}
-
-
-
